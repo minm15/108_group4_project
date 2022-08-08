96bea03211460f3174d884dbf69d56cb3f11fd4c
aoi123 <s960215@bles.tp.edu.tw>
2022-08-08 12:20:03 +0800

    change mailbox format

---
+++ src/component/test.js
@@ -0,0 +1,30 @@
+//仍在測試中
+
+//import React from "react";
+//import { palette } from '@material-ui/system';
+import { createTheme } from '@mui/material/styles';
+
+//setting
+/*
+const Chiptheme = createTheme({
+    components: {
+      MuiChip: {
+        variants: [
+          {
+            //props: { variant: 'dashed' },
+            
+            style: {
+                bgcolor: "#757575",
+                color: "info",
+                size:"small"
+            },
+          },
+          
+        ],
+      },
+    },
+  });
+
+  export default Chiptheme;
+
+*/
