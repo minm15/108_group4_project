41f60867aef04b5a43bebdfaa4e0abf45114a066
aoi123 <s960215@bles.tp.edu.tw>
2022-08-08 12:01:03 +0800

    use wrong branch

--- src/Letter/letter_grid.js
+++ src/Letter/letter_grid.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link, Outlet } from "react-router-dom";
-import { Box, Chip } from '@mui/material';
+import { Box, Chip, Grid} from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+
 // import { get_letter_list } from '../test/mail.js';
 import get_letter_list from '../data/letter_list';
 import MenuList from './menu';
-
-
+//import Chiptheme from '../component/test'
+//import { Component } from 'react';
+//import styled from 'styled-components'
+//import { ThemeProvider } from  '@mui/material/styles';
 
 const LetterGrid = () => {
     const columns = [
@@ -17,13 +20,28 @@ const LetterGrid = () => {
             renderCell: (params) => {
                 return (
                     <div>
+                       
                         {params.row.sender}
                         <Chip
                             label={params.row.sender_type}
                             size="small"
-                            sx={{ bgcolor: "#757575", color: "#F5F5F5" }}
+                            sx={{  color: "info" }}//bgcolor: "#757575",color:"#F5F5F5"
                         />
                     </div>
+                    /*<div>
+                        {params.row.sender}
+                        <ThemeProvider theme={Chiptheme}>
+                          
+                            <Chip
+                                label={params.row.sender_type}
+                                
+                           
+                            />
+       
+                        </ThemeProvider>
+
+                        
+                    </div>*/
                 )
             }
         },
@@ -55,21 +73,35 @@ const LetterGrid = () => {
     const rows = get_letter_list();
 
     return (
+               
         <div className='main_frame'>
-            <div className="menu">
-                <MenuList />
-            </div>
-            <Box sx={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={10}
-                    rowsPerPageOptions={[10]}
-                    componentsProps={{
-                        columnMenu: { background: 'red', counter: rows.length },
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="flex-start"
+                spacing='5'
+                >
+               <div className="menu">
+               <Grid item xs={12}>
+                    <MenuList />
+                </Grid>
+                
+                </div>
+                <Grid item xs={8}>
+                    <Box sx={{ height: 400, width: '100%' }}>
+                         <DataGrid
+                            rows={rows}
+                            columns={columns}
+                            pageSize={10}
+                            rowsPerPageOptions={[10]}
+                            componentsProps={{
+                                columnMenu: { background: 'red', counter: rows.length },
                     }}
                 />
-            </Box>
+                    </Box>
+                 </Grid>
+            </Grid>
             <Outlet />
         </div>
     )
