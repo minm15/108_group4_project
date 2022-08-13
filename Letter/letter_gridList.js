1db0eb1a72444a7f993465fd751d288aac2152f3
patricia96084 <58579511+patricia96084@users.noreply.github.com>
2022-08-13 11:57:20 +0800

    把datagrid改成grid
    註解仍有保留datagrid的部分，看弄css的哪個方便

---
+++ Letter/letter_gridList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Grid, Chip } from '@mui/material';
+import get_letter_list from "../data/letter_list";
+
+function LetterList() {
+    const letterList = get_letter_list();
+    return (
+        <Grid container>
+            {
+                letterList.map(
+                    (letter) => {
+                        return (
+                            <a href={`/letter_list/${letter.id}`}>
+                                <Grid container className={letter.id}>
+                                    <Grid item>
+                                        {letter.sender}
+                                        <Chip label={letter.sender_type} size="small" />
+                                    </Grid>
+                                    <Grid item>
+                                        【{letter.letter_type}】{letter.title}
+                                        <div classname="expired">{letter.expired}</div>
+                                    </Grid>
+                                    <Grid item>{letter.time}</Grid>
+                                </Grid>
+                            </a>
+                        )
+                    }
+                )
+            }
+        </Grid>
+    )
+}
+
+export default LetterList;
