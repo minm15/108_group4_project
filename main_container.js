1afc36689f468ab86a44138df67d34bcf70e74f3
patricia96084 <58579511+patricia96084@users.noreply.github.com>
2022-08-25 12:08:19 +0800

    Add files via upload

---
+++ main_container.js
@@ -0,0 +1,73 @@
+import React from "react";
+import LetterGrid from "./Letter/letter_grid";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import LetterReceive from "./Letter/letter_receive";
+import LetterWriting from "./Letter/letter_writing";
+import Manufactor from "./manufactor/mf_list";
+import Product from "./manufactor/mf_product";
+import LetterReply from "./Letter/letter_reply";
+
+class MainContainer extends React.Component {
+    render() {
+        return (
+            <div className="main_frame">
+                <div className="letter_frame">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={
+                                <div>
+                                    <Link to={`/letter_list`}>
+                                        Click here to go to mail page.
+                                    </Link>
+                                    <Link to={`/manufactory`}>
+                                        Click here to go to manufactory page.
+                                    </Link>
+                                </div>
+                            } />
+                            <Route path='/letter_list'>
+                                <Route index element={<LetterGrid />} />
+                                <Route
+                                    path=":letterId"
+                                    element={
+                                        <LetterReceive
+                                            user={{
+                                                name: 'takodachi公司',
+                                                type: 'manufacturer'
+                                            }}
+                                        />
+                                    }
+                                />
+                            </Route>
+                            <Route path="/letter_writing">
+                                <Route index element={
+                                    <LetterWriting
+                                        user={{
+                                            name: 'takodachi公司',
+                                            type: 'manufacturer'
+                                        }}
+                                    />
+                                } />
+                                <Route
+                                    path=":letterId"
+                                    element={
+                                        <LetterReply
+                                            user={{
+                                                name: 'takodachi公司',
+                                                type: 'manufacturer'
+                                            }}
+                                        />
+                                    } />
+                            </Route>
+                            <Route path="/manufactory">
+                                <Route index element={<Manufactor />} />
+                                <Route path="product" element={<Product />} />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MainContainer;
